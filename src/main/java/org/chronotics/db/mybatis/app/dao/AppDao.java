e0ceb5cee7b4291b2bb980d56a3faa84233de679
sglee <sg10.lee@gmail.com>
2018-05-23 11:02:10 +0900

    update mapper and properties for MySql connection

--- src/main/java/org/chronotics/db/mybatis/app/dao/AppDao.java
+++ src/main/java/org/chronotics/db/mybatis/app/dao/AppDao.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import javax.annotation.Resource;
 
 import org.apache.ibatis.session.SqlSession;
+import org.chronotics.db.mybatis.MapperMySql;
+import org.chronotics.db.mybatis.SqlStatement;
 import org.chronotics.db.mybatis.app.mapper.IAppMapper;
 import org.springframework.stereotype.Repository;
 
@@ -15,8 +17,8 @@ public class AppDao implements IAppDao {
 	@Resource(name = "sqlSessionSimpleMySql")
 	private SqlSession sqlSession;
 	
-//	@Resource(name = "mapperSimpleMySql")
-//	private MapperMySql mapper;
+	@Resource(name = "mapperSimpleMySql")
+	private MapperMySql mapper;
 	
 	public void setSqlSession(SqlSession _sqlSession) {
 		sqlSession = _sqlSession;
@@ -28,30 +30,37 @@ public class AppDao implements IAppDao {
 			return 888;
 		}
 		
-//		if(mapper == null) {
-//			return 889;
+		if(mapper == null) {
+			return 889;
+		}
+		SqlStatement sqlSatement = 
+				new SqlStatement.Builder()
+				.select("*")
+				.from("user")
+				.build();
+
+		Map<String, Object> result = 
+				mapper.selectOne(sqlSatement);
+//		if(result==null) {
+//			System.out.println("result is null");
+//			return 887;
 //		}
 		
-//		SqlStatement sqlSatement = 
-//				new SqlStatement.Builder()
-//				.select("*")
-//				.from("user")
-//				.build();
+//		Map<Object, Object> parameter = new LinkedHashMap<Object, Object>();
+//		parameter.put("table", "user");
+//		parameter.put("name", name);
+//		
+//		Map<String, Object> result =
+//				sqlSession.selectOne(IAppMapper.className+"selectOne",parameter);
 //
-//		Map<String, Object> result = 
-//				mapper.selectOne(sqlSatement);
-		
-		Map<Object, Object> parameter = new LinkedHashMap<Object, Object>();
-		parameter.put("table", "user");
-		parameter.put("name", name);
-		
-		Map<String, Object> result =
-				sqlSession.selectOne(IAppMapper.className+"selectOne",parameter);
-
+		if(result==null) {
+			System.out.println("result is null");
+			return 887;
+		}
 		if(result.size() ==0) {
 			return 999;
 		} else {
-			float number = (float)(result.get("name"));
+			float number = (float)(result.get("number"));
 			return number;
 		}
 //		return 0;
