c837e56adf9964577e7f6891f1641627be6b7748
sglee <sg10.lee@gmail.com>
2018-06-22 20:36:53 +0900

    update Application.java and test .gitignore

--- src/main/java/org/chronotics/db/mybatis/app/Application.java
+++ src/main/java/org/chronotics/db/mybatis/app/Application.java
@@ -15,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 	
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(Application.class,args);
+		ApplicationContext context = 
+				SpringApplication.run(Application.class,args);
 	}
 }
